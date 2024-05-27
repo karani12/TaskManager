f48a4bf3c5a72213cc5683b3cacbdbd9877bb967
karani12 <karanijohn018@gmail.com>
2024-05-27 22:07:31 +0300

    feat: dashboard ui components

---
+++ resources/js/Components/Dashboard/StatCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { defineProps, computed } from 'vue';
+
+const props = defineProps<{
+    name: string;
+    number: number;
+    class?: string;
+}>();
+console.log(props);
+const base = 'px-4 min-w-28  rounded-md flex flex-col ';
+const classes = computed(() => {
+    return `${base} ${props.class || ''}`;
+});
+
+
+
+</script>
+
+<template>
+    <div :class="classes">
+        <p class="text-gray-400 font-bold">{{ props.name }}</p>
+        <h1 class="number text-4xl font-black">{{ props.number }}</h1>
+        <slot name="sub-stats"></slot>
+      
+    </div>
+</template>
---
+++ resources/js/Components/Dashboard/Stats.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import StatsCard from './StatCard.vue';
+</script>
+<template>
+    <section class="flex justify-between">
+
+        <div class="avatar flex space-x-3 items-center justify-center">
+            <img :src="$page.props.auth.user.avatar" alt="avatar" class="rounded-full w-16 h-16" />
+            <div class="name">
+                <h3 class="text-2xl font-bold">{{ $page.props.auth.user.name }}</h3>
+                <p class="text-base text-gray-500">{{ $page.props.auth.user.email }}</p>
+            </div>
+        </div>
+        <div class="stats flex space-x-3">
+            <StatsCard name="Pending" :number="10" class="bg-warning-50 justify-between" >
+                <template #sub-stats>
+                    <p class="text-gray-400 mt-5">Sub Stats</p>
+                </template>
+            </StatsCard>
+            <StatsCard name="Completed" :number="20" class="bg-success-50" />
+            <StatsCard name="Total" :number="30" class="bg-gray-50" />
+        </div>
+    </section>
+
+</template>
